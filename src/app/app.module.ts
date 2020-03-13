926ab14f9069200597aafab87de6e3c5f98b063c
rndcastro <raphaelncastro@gmail.com>
2020-03-13 16:35:57 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { CartServiceService } from './cart-service.service';
+import { CartService } from './cart.service';
 
 
 registerLocaleData(localePt);
